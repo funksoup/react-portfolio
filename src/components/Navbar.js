e063967ecde4a9757c51195a390187065996385f
funksoup <josephine@funksoup.com>
2020-06-26 23:28:38 -0400

    added sliding menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
+import MobileRightMenuSlider from '@material-ui/core/Drawer';
 
 import {
 	AppBar,
@@ -35,13 +35,16 @@ const useStyles = makeStyles(theme=> ({
 	menuSliderContainer: {
 		width: 250,
 		background: "#511",
-		height: "30rem"
+		height: "100%"
 	},
 	avatar: {
 		display: "block",
 		margin: "0.5rem auto",
 		width: theme.spacing(13),
 		height: theme.spacing(13)
+	},
+	listItem: {
+		color: "tan"
 	}
 }));
 
@@ -67,30 +70,57 @@ const menuItems = [
 
 
 const Navbar = () => {
-	const classes = useStyles()
-	return (
-		<>
-		<Box className={classes.menuSliderContainer} component="div">
+	const [state, setState] = useState({
+		right: false
+	});
+
+	const toggleSlider = ((slider, open) => () => {
+		setState({...state, [slider]: open});
+	});	
+
+	const classes = useStyles();
+
+	const sideList = slider => (
+
+		<Box 
+		className={classes.menuSliderContainer} 
+		component="div"
+		onClick={toggleSlider(slider, false)}
+		>
 			<Avatar className={classes.avatar} src={avatar} alt="Josephine's avatar" />
 			<Divider />
 			<List>
 				{menuItems.map((lsItem, key)=> (
 				<ListItem button key={key}>
-					<ListItemIcon>{lsItem.listIcon}</ListItemIcon>
-					<ListItemText primary={lsItem.listText} />
+					<ListItemIcon className={classes.listItem}>{lsItem.listIcon}</ListItemIcon>
+					<ListItemText className={classes.listItem} primary={lsItem.listText} />
 				</ListItem>
 				))}
 			</List>
 		</Box>
+		)
+
+	return (
+		<>
+			
 		<Box component="nav">
 			<AppBar position="static" style={{background: "#222"}}>
 				<Toolbar>
-					<IconButton>
+					<IconButton onClick={toggleSlider("right", true)}>
 						<Menu style={{color: "tomato"}} />
 					</IconButton>
 					<Typography variant="h5" style={{color: "tan"}}>
 					Portfolio
 					</Typography>
+
+					<MobileRightMenuSlider 
+					anchor="left"
+					open={state.right}
+					onClose={toggleSlider("right", false)}
+					>
+					{sideList("right")}
+					</MobileRightMenuSlider>
+
 				</Toolbar>
 			</AppBar>
 		</Box>
